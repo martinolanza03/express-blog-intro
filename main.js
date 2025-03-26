cc73ef1e8e02123b87b79859eea21fd6428e1aa4
martinolanza03 <lanzamartino3@gmail.com>
2025-03-26 15:40:31 +0100

    feat: import array in the main.js

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const plateBlog = require('./array.js')
+
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
+})
+
+app.get('/bacheca', (req, res) => {
+    res.send(plateBlog);
 })
 
 app.listen(port, () => {
