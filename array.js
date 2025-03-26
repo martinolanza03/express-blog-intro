90fa4199ad8114f4a2b662126316f609529bad7c
martinolanza03 <lanzamartino3@gmail.com>
2025-03-26 16:41:19 +0100

    fix: fix syntax in image

--- array.js
+++ array.js
@@ -2,7 +2,7 @@ const plateBlog = [
     {
         title: "Ciambellone",
         content: "Ciambellone con zucchero",
-        image: "http://127.0.0.1:3000/img/ciambellone.jpeg",
+        image: "/img/ciambellone.jpeg",
         tag: [
             "ciambellone",
             "zucchero"
@@ -11,7 +11,7 @@ const plateBlog = [
     {
         title: "Cracker di barbabietola",
         content: "Tagliere con cracker di barbabietola",
-        image: "http://127.0.0.1:3000/img/cracker_barbabietola.jpeg",
+        image: "/img/cracker_barbabietola.jpeg",
         tag: [
             "cracker",
             "barbabietola"
@@ -20,7 +20,7 @@ const plateBlog = [
     {
         title: "Pane fritto dolce",
         content: "Pane fritto dolce con zucchero",
-        image: "http://127.0.0.1:3000/img/pane_fritto_dolce.jpeg",
+        image: "/img/pane_fritto_dolce.jpeg",
         tag: [
             "pane",
             "zucchero"
@@ -29,7 +29,7 @@ const plateBlog = [
     {
         title: "Pasta di barbabietole",
         content: "Piatto con pasta di barbabietole",
-        image: "http://127.0.0.1:3000/img/pasta_barbabietola.jpeg",
+        image: "/img/pasta_barbabietola.jpeg",
         tag: [
             "pasta",
             "barbabietole"
@@ -38,7 +38,7 @@ const plateBlog = [
     {
         title: "Torta paesana",
         content: "Piatto con torta paesana",
-        image: "http://127.0.0.1:3000/img/torta_paesana.jpeg",
+        image: "/img/torta_paesana.jpeg",
         tag: [
             "Torta",
             "zucchero"
