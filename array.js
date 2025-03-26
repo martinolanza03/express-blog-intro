4bac57b00cbc63881a6c9a35e63e00d749e6e8e5
martinolanza03 <lanzamartino3@gmail.com>
2025-03-26 15:32:53 +0100

    feat: create file with array and export the array

---
+++ array.js
@@ -0,0 +1,49 @@
+const plateBlog = [
+    {
+        title: "Ciambellone",
+        content: "Ciambellone con zucchero",
+        image: "img/ciambellone.jpg",
+        tag: [
+            "ciambellone",
+            "zucchero"
+        ]
+    },
+    {
+        title: "Cracker di barbabietola",
+        content: "Tagliere con cracker di barbabietola",
+        image: "img/cracker_barbabietola.jpg",
+        tag: [
+            "cracker",
+            "barbabietola"
+        ]
+    },
+    {
+        title: "Pane fritto dolce",
+        content: "Pane fritto dolce con zucchero",
+        image: "img/pane_fritto_dolce.jpeg",
+        tag: [
+            "pane",
+            "zucchero"
+        ]
+    },
+    {
+        title: "Pasta di barbabietole",
+        content: "Piatto con pasta di barbabietole",
+        image: "img/pasta_barbabietola.jpeg",
+        tag: [
+            "pasta",
+            "barbabietole"
+        ]
+    },
+    {
+        title: "Torta paesana",
+        content: "Piatto con torta paesana",
+        image: "img/torta_paesana.jpeg",
+        tag: [
+            "Torta",
+            "zucchero"
+        ]
+    },
+];
+
+module.exports = plateBlog;
