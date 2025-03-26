e65cd050b0a8002bfd89bbedac4b7023ec707a8e
martinolanza03 <lanzamartino3@gmail.com>
2025-03-26 15:34:37 +0100

    fix: fix route /

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (res, req) => {
-    console.log('Server del mio blog');
+app.get('/', (req, res) => {
+    res.send('Server del mio blog');
 })
 
 app.listen(port, () => {
